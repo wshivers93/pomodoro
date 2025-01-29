a9c57a74fad862a0f8c163ce60bfe5fcd525dcaa
Will Shivers <tayloe93@gmail.com>
2025-01-29 16:08:25 -0600

    Fix version number

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "pomodoro",
 	Short: "Simple timer for the pomodoro time management method",
-	Version: "0.0.1",
+	Version: "0.1.0",
 }
 
 func Execute() error {
